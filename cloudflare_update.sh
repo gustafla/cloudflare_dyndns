7ea8e3d4dfc8c1af16fe7c58a16c59cff55c2582
Lauri Gustafsson <lauri.gustafsson@iki.fi>
2024-10-02 17:20:13 +0300

    cloudflare_update.sh: wip

--- cloudflare_update.sh
+++ cloudflare_update.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu
 
+#https://developers.cloudflare.com/api-next/resources/dns/subresources/records/methods/edit/
+api="https://api.cloudflare.com/client/v4/zones"
 
 ident_a_addr="$(curl -sS -4 https://ident.me)"
 echo cur $ident_a_addr
@@ -11,24 +13,29 @@ echo cur $ip_aaaa_addr
 
 dns=($NS)
 dns_server=${dns[$RANDOM % ${#dns[@]}]}
-#TODO replace dig with Cloudflare API
 echo sending dns queries to $dns_server
-for zone in $ZONES; do
-  dns_a_record="$(dig +short @$dns_server $zone A)"
-  echo dns $dns_a_record \($zone\)
-  if [[ "$dns_a_record" != "$ident_a_addr" ]]; then
-    echo updating
-    #curl -4sS "https://dynv6.com/api/update?zone=$zone&token=$TOKEN&ipv4=$ident_a_addr"
-  else
-    echo no action
-  fi
 
-  dns_aaaa_record="$(dig +short @$dns_server $zone AAAA)"
-  echo dns $dns_aaaa_record \($zone\)
-  if [[ "$dns_aaaa_record" != "$ip_aaaa_addr" ]]; then
-    echo updating
-    #curl -sS "https://dynv6.com/api/update?zone=$zone&token=$TOKEN&ipv6prefix=auto&ipv6=$ip_aaaa_addr"
-  else
-    echo no action
-  fi
-done
+#TODO replace dig with Cloudflare API
+dns_a_record="$(dig +short @$dns_server $zone A)"
+echo dns $dns_a_record \($zone\)
+if [[ "$dns_a_record" != "$ident_a_addr" ]]; then
+  echo updating
+  #curl -4sS "https://dynv6.com/api/update?zone=$zone&token=$TOKEN&ipv4=$ident_a_addr"
+  curl -sS -X PATCH --data-binary @- -H "Authorization: Bearer $TOKEN" -H "Content-Type:application/json" "$api/$ZONE/dns_records/$RECORD_A" <<EOF
+{
+  "content": "$ident_a_addr"
+}
+EOF
+else
+  echo no action
+fi
+
+#TODO replace dig with Cloudflare API
+dns_aaaa_record="$(dig +short @$dns_server $zone AAAA)"
+echo dns $dns_aaaa_record \($zone\)
+if [[ "$dns_aaaa_record" != "$ip_aaaa_addr" ]]; then
+  echo updating
+  #curl -sS "https://dynv6.com/api/update?zone=$zone&token=$TOKEN&ipv6prefix=auto&ipv6=$ip_aaaa_addr"
+else
+  echo no action
+fi
